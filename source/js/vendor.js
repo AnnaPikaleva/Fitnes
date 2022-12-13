d8eaaa6c30eb9983be34999af6c2c2bb6ab0f9b7
AnnaPikaleva <97855987+AnnaPikaleva@users.noreply.github.com>
2022-12-13 19:40:44 +0300

    Swiper

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,43 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
+import './vendor/swiper.js';
+// import Swiper from './vendor/swiper.js';
+// import './vendor/focus-visible-polyfill';
+
+// const sliderCoaches = () => {
+//   const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+// };
+
+// export {sliderCoaches};
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  allowTouchMove: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    nextEl: '.slider__next',
+    prevEl: '.slider__prev',
+  },
+});
